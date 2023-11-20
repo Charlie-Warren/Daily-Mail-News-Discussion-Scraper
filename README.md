0c7497659a34492e0d48005e0149a9f82c3d4e76
Charlie-Warren <c_warren1@live.com>
2023-11-20 13:23:13 +0000

    added files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Daily-Mail-News-Discussion-Scraper
 Retreives and parses comments from Daily Mail articles.
+
+## Requirements
+This script requires pandas, which can be installed by running:
+```
+python -m pip install pandas
+```
+
+## Usage
+Ensure you are in the right directory and run:
+```
+python scraper.py -u "https://www.dailymail.co.uk/news/article-12770219/Shakira-agrees-deal-avoid-tax-fraud-trial-Spain.html"
+```
---
+++ scraper.py
@@ -0,0 +1,89 @@
+import json
+from pathlib import Path
+import argparse
+
+import pandas as pd
+
+
+SCRIPT_DIR = Path(__file__).parent.absolute()
+DATA_PATH = SCRIPT_DIR / "data.json"
+
+
+def get_id(url:str):
+    sections = url.split("/")
+    for section in sections:
+        if "article" in section:
+            return section.split("-")[1]
+
+
+def get_comments_url(article_id, max_comments:int=500) -> str:
+    url = f"https://www.dailymail.co.uk/reader-comments/p/asset/readcomments/{article_id}?max={max_comments}&order=desc"
+    return url
+
+
+def read_json() -> dict:
+    try:
+        with open(DATA_PATH) as f:
+            data = json.load(f)
+    except:
+        print(f"Couldn't read data from {DATA_PATH}.")
+    return data
+
+
+def flatten_comments(comment, data_list:list) -> None:
+    # Add the comment to the data_list
+    data_list.append({
+        'userAlias': comment['userAlias'],
+        'assetHeadline': comment['assetHeadline'],
+        'userLocation': comment['userLocation'],
+        'formattedDateAndTime': comment['formattedDateAndTime'],
+        'assetId': comment['assetId'],
+        'voteCount': comment['voteCount'],
+        'id': comment['id'],
+        'userIdentifier': comment['userIdentifier'],
+        'hasProfilePicture': comment['hasProfilePicture'],
+        'voteRating': comment['voteRating'],
+        'dateCreated': comment['dateCreated'],
+        'assetCommentCount': comment['assetCommentCount'],
+        'assetUrl': comment['assetUrl'],
+        'message': comment['message'],
+    })
+
+    # Recursively add replies
+    if 'replies' in comment and 'comments' in comment['replies']:
+        for reply in comment['replies']['comments']:
+            flatten_comments(reply, data_list)
+
+
+def comments_parser(json_data:dict) -> pd.DataFrame:
+    page_data = json_data['payload']['page']
+    data_list = []
+    for comment in page_data:
+        flatten_comments(comment, data_list)
+
+    df = pd.DataFrame(data_list)
+    df.drop(columns=['assetHeadline', 'assetId', 'assetUrl', 'assetCommentCount'], inplace=True)
+    return df
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Get comments from Daily Mail articles.")
+    parser.add_argument("-u", "--url", type=str, required=True, help="URL of the article")
+    args = parser.parse_args()
+    myurl: str = args.url
+    id = get_id(myurl)
+    if id is not None:
+        comm_url = get_comments_url(id, 1000)
+        print(comm_url)
+        print("Go to this url and select raw data. Copy and paste all the data into \"data.json\".")
+        input("Then press Enter to continue...")
+        data = read_json()
+        mydf = comments_parser(data)
+        print(f"Data preview:\n{mydf.head()}")
+        print(f"Comments parsed: {mydf.shape[0]}")
+    else:
+        print("Couldn't find Article ID")
+
+
+if __name__ == "__main__":
+    main()
